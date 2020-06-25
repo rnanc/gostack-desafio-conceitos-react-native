64bc1badbe8c9118c1439a57e2b1a9ba7d8f988f
Renan <renanfigcarneiro@gmail.com>
2020-06-25 20:41:24 -0300

    done

--- src/App.js
+++ src/App.js
@@ -55,7 +55,6 @@ export default function App() {
               <View style={styles.likesContainer}>
                 <Text
                   style={styles.likeText}
-                  // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
                   testID={`repository-likes-${repository.id}`}
                 >
                   {repository.likes} curtidas
@@ -65,8 +64,7 @@ export default function App() {
               <TouchableOpacity
                 style={styles.button}
                 onPress={() => handleLikeRepository(repository.id)}
-                // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
-                testID={`repository-likes-${repository.id}`}
+                testID={`like-button-${repository.id}`}
               >
                 <Text style={styles.buttonText}>Curtir</Text>
               </TouchableOpacity>
